19c61ab6052ea4c6875eedadc3ea3bf91b02bc35
Sanat Kumar Dubey <studentia312@gmail.com>
2022-05-20 21:35:35 +0530

    fixed bugs

--- src/Codec/Decoder.java
+++ src/Codec/Decoder.java
@@ -1,7 +1,6 @@
 package Codec;
 
 import com.google.common.collect.BiMap;
-
 public class Decoder {
     // Inputs encoded message, decodes it using huffman table then return the decoded message
     public static String decode(String message, BiMap<Character, String> huffmanTable) {
